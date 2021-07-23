1410b51bd111b41e23b40077de44121bfaf3c517
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2021-07-23 11:32:20 +0900

    Implement WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,56 @@
 #include <Arduino.h>
-#include <log/Log.h>
+#include <WiFi.h>
 
-const int LED_PIN = 2;
+#include <log/Log.h>
+#include "Config.h"
+
+const int LED_PIN            = 2;
+const int HIGH_INTERVAL_MS   = 250;
+const int NORMAL_INTERVAL_MS = 500;
+
+static void showConnectingWifi()
+{
+    digitalWrite(LED_PIN, HIGH);
+    delay(HIGH_INTERVAL_MS);
+    digitalWrite(LED_PIN, LOW);
+    delay(HIGH_INTERVAL_MS);
+}
+
+static void reconnectWifi()
+{
+    // If forget mode(WIFI_STA), mode might be WIFI_AP_STA.
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASS);
+
+    Log::Info("WiFi connecting...");
+    while (WiFi.status() != WL_CONNECTED) {
+        showConnectingWifi();
+        Log::Debug(".");
+    }
+
+    Log::Info("WiFi connected.");
+    String log = "IP address: ";
+    IPAddress ip = WiFi.localIP();
+    log += ip[0]; log += ".";
+    log += ip[1]; log += ".";
+    log += ip[2]; log += ".";
+    log += ip[3];
+    Log::Info(log.c_str());
+}
 
 void setup()
 {
+    pinMode(LED_PIN, OUTPUT);
+
     Serial.begin(115200);
     Log::Info("Hello ESP32 World!");
-    pinMode(LED_PIN, OUTPUT);
+    reconnectWifi();
 }
 
 void loop()
 {
     digitalWrite(LED_PIN, HIGH);
-    delay(500);
+    delay(NORMAL_INTERVAL_MS);
     digitalWrite(LED_PIN, LOW);
-    delay(500);
+    delay(NORMAL_INTERVAL_MS);
 }
