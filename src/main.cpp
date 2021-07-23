6207acb49d964f992157baaa32ef3af18024d6b3
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2021-07-23 11:20:43 +0900

    Add log library

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
+#include <log/Log.h>
 
 const int LED_PIN = 2;
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Hello ESP32 World!");
+    Log::Info("Hello ESP32 World!");
     pinMode(LED_PIN, OUTPUT);
 }
 
